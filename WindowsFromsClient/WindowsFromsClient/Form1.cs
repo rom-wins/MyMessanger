c024764c87cd7929e3903f6708aac6617f42e2b6
rom-wins <chetverikov_roman@inbox.ru>
2022-06-16 14:37:14 +0800

    Реализован асинхронный клиент с помощью Windows Forms

--- WindowsFromsClient/WindowsFromsClient/Form1.cs
+++ WindowsFromsClient/WindowsFromsClient/Form1.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MyMessanger;
+using System;
 using System.Windows.Forms;
+using System.Threading.Tasks;
+
 
 namespace WindowsFromsClient
 {
     public partial class Form1 : Form
     {
+        private static int MessageID = 0;
+        private static string UserName;
+        private static string Message;
+        private static MessangerClientAPI API = new MessangerClientAPI();
         public Form1()
         {
             InitializeComponent();
@@ -24,7 +24,31 @@ namespace WindowsFromsClient
 
         private void SendButton_Click(object sender, EventArgs e)
         {
-            MessageTB.Text = UserNameTB.Text;
+            UserName = UserNameTB.Text;
+            Message = MessageTB.Text;
+
+            if ((UserName.Length > 1) && (Message.Length > 1))
+            {
+                MyMessanger.Message msg = new MyMessanger.Message(UserName, Message, DateTime.Now);
+                API.SendMessage(msg);
+            }
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var getMessages = new Func<Task>(async () =>
+            {
+                MyMessanger.Message msg = await API.GetMessageHTTPAsync(MessageID);
+                while (msg != null)
+                {
+                    MessagesLB.Items.Add(msg);
+                    MessageID++;
+                    
+                    msg = await API.GetMessageHTTPAsync(MessageID);
+                }
+            });
+            getMessages.Invoke();
 
         }
     }
---
+++ WindowsFromsClient/WindowsFromsClient/MessangerClientAPI.cs
@@ -0,0 +1,50 @@
+﻿
+using System.IO;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyMessanger
+{
+    class MessangerClientAPI
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task<Message> GetMessageHTTPAsync(int MessageID)
+        {
+
+            var responceString = await client.GetStringAsync("http://localhost:5000/api/Messanger/" + MessageID.ToString());
+            if (responceString != null)
+            {
+                Message deserializedMsg = JsonConvert.DeserializeObject<Message>(responceString);
+                return deserializedMsg;
+            }
+
+            return null;
+        }
+        public bool SendMessage(Message msg)
+        {
+            WebRequest request = WebRequest.Create("http://localhost:5000/api/Messanger");
+            request.Method = "POST";
+            string postData = JsonConvert.SerializeObject(msg);
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            request.ContentType = "application/json";
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            WebResponse response = request.GetResponse();
+            dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader (dataStream);
+            string responceFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            return true;
+
+        }
+    }
+}
